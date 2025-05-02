5208311482dd55f8075a0572a0e3b7424576b241
lorella-marino <lorellamarino@gmail.com>
2025-05-02 10:20:15 +0200

    Initial commit

---
+++ src/main/java/it/epicode/s7_l4/S7L4Application.java
@@ -0,0 +1,13 @@
+package it.epicode.s7_l4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7L4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7L4Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/s7_l4/S7L4ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.s7_l4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7L4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
