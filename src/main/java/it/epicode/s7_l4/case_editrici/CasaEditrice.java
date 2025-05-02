c0afc5f89da5af5e62c2233354672b5b01c46729
lorella-marino <lorellamarino@gmail.com>
2025-05-02 10:32:06 +0200

    aggiunta classe ClasseEditrice

---
+++ src/main/java/it/epicode/s7_l4/case_editrici/CasaEditrice.java
@@ -0,0 +1,10 @@
+package it.epicode.s7_l4.case_editrici;
+
+import lombok.Data;
+
+@Data
+public class CasaEditrice {
+    private String nome;
+    private String indirizzo;
+    private String telefono;
+}
